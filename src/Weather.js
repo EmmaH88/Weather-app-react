63f5e9d56415b7e0b05cac581f20fdb600adece0
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2022-12-27 20:18:41 +0000

    created formattedDate component

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -13,7 +14,7 @@ export default function Weather(props) {
       feelsLike: Math.round(response.data.main.feels_like),
       city: response.data.name,
       description: response.data.weather[0].description,
-      date: "Tuesday 19:45",
+      date: new Date(response.data.dt * 1000),
       iconUrl: `https://ssl.gstatic.com/onebox/weather/64/cloudy.png`,
     });
     console.log(response.data);
@@ -31,7 +32,9 @@ export default function Weather(props) {
         <h2>The weather in {weatherData.city} is...</h2>
 
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
 
