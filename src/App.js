ce5518cf5440b2cfd68b6201592a0e56fffe0765
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2022-12-27 15:43:20 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <footer>
-        this page is hosted on
-        <a
-          href="https://github.com/EmmaH88/Weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>React Weather App</h1>
+        <button className="btn btn-primary">Hello</button>
+
+        <footer>
+          This page is open-sourced on{" "}
+          <a
+            href="https://github.com/EmmaH88/Weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          {""}
+          hosted on{" "}
+          <a href="https://rad-stardust-3ab6b7.netlify.app">Netlify</a>
+          {""} and coded by Emma H 😍
+        </footer>
+      </div>
     </div>
   );
 }
