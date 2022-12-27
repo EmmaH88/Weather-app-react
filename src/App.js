bc71184e9ba2fbf40642a50f3124ceeaab62b2d2
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2022-12-27 15:15:10 +0000

    removed un-needed files and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>React Weather App</h1>
+      <footer>
+        this page is hosted on
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/EmmaH88/Weather-app-react"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
